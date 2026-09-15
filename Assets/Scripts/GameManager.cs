565eda1b7ccfaeec43e7ccc169591a119067df39
Citlalli-AraujoG <citlalliaraujo023@gmail.com>
2026-09-15 13:28:25 -0500

    Little organization and Started Scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject Ring1;
+    [SerializeField] GameObject Ring2;
+    [SerializeField] GameObject Ring3;
+    [SerializeField] GameObject Ring4;
+    [SerializeField] GameObject Ring5;
+
+    int level = 1;
+    float accuracyPercentage = 0;
+
+
+    void 
+
+    
+}
